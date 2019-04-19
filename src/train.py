b5c248563ca57d7a8c9d947ff90306ae75e948d8
Jeremy Bouhi <jeremybh2406@gmail.com>
2019-04-19 10:00:03 +0200

    using-decision-tree-regressor

--- src/train.py
+++ src/train.py
@@ -1,4 +1,4 @@
-from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
 import conf
 import pickle
 
@@ -10,7 +10,7 @@ with open(conf.X_train, 'rb') as fd:
 with open(conf.y_train, 'rb') as fd:
     y_train = pickle.load(fd)
 
-reg = LinearRegression()
+reg = DecisionTreeRegressor()
 reg.fit(X_train, y_train)
 
 print(reg)
